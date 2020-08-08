b412ba9724255e2ef86a131731fa2981dc20d21e
aomikan10 <mm1021201@gmail.com>
2020-08-09 00:33:00 +0900

    Update sending_destination.rb

--- app/models/sending_destination.rb
+++ app/models/sending_destination.rb
@@ -1,4 +1,4 @@
 class SendingDestination < ApplicationRecord
   belongs_to :user
-  validates :post_code, :prefecture
+  validates :destination_first_name, :destination_family_name, :destination_first_name_kana, :destination_family_name_kana, :post_code, :prefecture_code, :city, :house_number, :phone_number, presence: true
 end
