ac8409830afeb54f225ae5fec7f95a544f0282d2
aomikan10 <mm1021201@gmail.com>
2020-08-08 15:18:42 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -22,6 +22,39 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render :new_profile
   end
 
+  def create_profile
+    @user = User.new(session["devise.regist_data"]["user"])
+    @profile = Profile.new(profile_params)
+    unless @profile.valid?
+      flash.now[:alert] = @profile.errors.full_messages
+      render :new_profile and return
+    end
+    @user.build_profile(@profile.attributes)
+    session["profile"] = @profile.attributes
+    @sending_destination = @user.build_sending_destination
+    render :new_sending_destination
+  end
+
+  protected
+
+  def profile_params
+    params.require(:profile).permit(first_name,family_name,first_name_kana,family_name_kana,birth_year,birth_month,birth_day,introduction,avatar,user_id)
+  end
+
+  def create_sending_destination
+    @user = User.new(session["devise.regist_data"]["user"])
+    @profile = Profile.new(session["profile"])
+    @sending_destination = Sending_destination.new(Sending_destination_params)
+    unless @sending_destination.valid?
+      flash.now[:alert] = @sending_destination.errors.full_messages
+      render :new_sending_destination and return
+    end
+    @user.build_profile(@profile.attributes)
+    @user.build_sending_destination(@sending_destination.attributes)
+    @user.save
+    sign_in(:user, @user)
+  end
+
   # GET /resource/edit
   # def edit
   #   super
