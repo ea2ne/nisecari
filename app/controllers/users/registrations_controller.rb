b6c04366e498ba14e6e50022ffd3355bf3177c9f
aomikan10 <mm1021201@gmail.com>
2020-08-09 02:58:54 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -35,16 +35,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render :new_sending_destination
   end
 
-  protected
-
-  def profile_params
-    params.require(:profile).permit(:first_name, :family_name, :first_name_kana, :family_name_kana, :birth_year, :birth_month, :birth_day, :introduction, :avatar, :user_id)
-  end
-
   def create_sending_destination
     @user = User.new(session["devise.regist_data"]["user"])
     @profile = Profile.new(session["profile"])
-    @sending_destination = Sending_destination.new(sending_destination_params)
+    @sending_destination = SendingDestination.new(sending_destination_params)
     unless @sending_destination.valid?
       flash.now[:alert] = @sending_destination.errors.full_messages
       render :new_sending_destination and return
@@ -52,11 +46,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
     @user.build_profile(@profile.attributes)
     @user.build_sending_destination(@sending_destination.attributes)
     @user.save
-    session["devise.regist_data"]["user"]["profile"].clear
     sign_in(:user, @user)
   end
 
   protected
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day, :introduction)
+  end
 
   def sending_destination_params
     params.require(:sending_destination).permit(:destination_first_name, :destination_family_name, :destination_first_name_kana, :destination_family_name_kana, :post_code, :prefecture_code, :city, :house_number, :phone_number)
