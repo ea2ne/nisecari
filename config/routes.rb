0a9cd62305e7a7aa3fc25efd4f516460ed12945e
aomikan10 <mm1021201@gmail.com>
2020-08-09 00:31:44 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'profiles', to: 'users/registrations#new_profile'
     post 'profiles', to: 'users/registrations#create_profile'
+    get 'sending_destination', to: 'users/registrations#new_sending_destination'
+    post 'sending_destination', to: 'users/registrations#create_sending_destination'
   end
   root 'items#index'
   resources :items
