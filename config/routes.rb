a36a8ecf12ac23f163addcc8d441d7da23ee5a67
ea2ne <ea2ne@yahoo.co.jp>
2020-08-11 11:48:15 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   root 'items#show'
   resources :items do
-  resources :favorites, only: [:create, :destroy]
-  collection do
-    get 'get_category_cildren', defaults:{ format: 'json'}
-    get 'get_category_grandcildren', defaults:{ format: 'json'}
-    get 'search'
-    get 'post_done'
-    get 'delete_done'
-    get 'detail_search'
-    get 'update_done'
+    resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    collection do
+      get 'get_category_cildren', defaults:{ format: 'json'}
+      get 'get_category_grandcildren', defaults:{ format: 'json'}
+      get 'search'
+      get 'post_done'
+      get 'delete_done'
+      get 'detail_search'
+      get 'update_done'
+    end
   end
 end
