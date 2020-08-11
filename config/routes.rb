cc8b061984f20d770893273aa7146c57b0ffdff3
oomae2469 <takalock@icloud.com>
2020-08-11 20:07:58 +0900

    Revert "root-modification"
    This reverts commit d19028cf52fd64df4e875be35bf3322aa1d58d8a.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   end
   root 'items#index'
   resources :items
-  resources :users, only: :show
-  resources :credit_cards, only: [:new, :create]
+  resources :users
+  resources :credit_cards
 end
