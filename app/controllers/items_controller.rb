729f4ac2df71f5dfb088b1548dc8cb7f78a31388
aomikan10 <mm1021201@gmail.com>
2020-08-13 18:48:38 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,6 +10,8 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
+      if item.user_id == current_user.id
+        item.destroy
+      end
   end
 end
