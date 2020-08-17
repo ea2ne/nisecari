68c495a6fe06ab01a0ee6ccb33e746067c40c8dc
ea2ne <ea2ne@yahoo.co.jp>
2020-08-17 12:55:58 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :item_images
   belongs_to :category
-  belongs_to user, foreign_key: 'user_id'
+  belongs_to :user, foreign_key: 'user_id'
   
 
   belongs_to :seller, class_name: "User"
