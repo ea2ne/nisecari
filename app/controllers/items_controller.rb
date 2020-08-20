bd334f570c69d4e16f972739fe03c2c8c166c4b0
hiromitsu004 <65520367+hiromitsu004@users.noreply.github.com>
2020-08-20 18:50:47 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:buy, :pay]
   def index
     @items = Item.all
-    # @items = Item.includes(:item_images).order('created_at DESC')
+    @items = Item.includes(:item_images).order('created_at DESC')
   end
 
   def new
