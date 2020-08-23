e86be2d3e98774c2c51a68e30b675b06bfcd8108
aomikan10 <mm1021201@gmail.com>
2020-08-23 13:04:36 +0900

    Update.view

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, foreign_key: 'user_id', optional:true
   
-
   belongs_to :seller, class_name: "User"
   belongs_to :buyer, class_name: "User", optional: true
   belongs_to :brand
