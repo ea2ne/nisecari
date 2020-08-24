036da0f3b69408ff609748f2d03ba3916f0f9913
aomikan10 <mm1021201@gmail.com>
2020-08-24 12:35:17 +0900

    Update sns_credential_spec.rb

--- spec/models/sns_credential_spec.rb
+++ spec/models/sns_credential_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
-describe User do
+describe SnsCredential do
   describe '#create' do
     it "プロバイダーが入っているか" do
-      user = build(:user)
-      expect(user).to be_valid
+      user = build(:sns_credential, provider: nil)
+      user.valid?
     end
+
+    it "uidが入っているか" do
+      user = build(:sns_credential, uid: nil)
+      user.valid?
+    end
+  end
+end
